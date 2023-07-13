09e2bf0efdec6011a39da7342776269a9402eef9
Timi12k <oluwatimilehinemmanuel62@gmail.com>
2023-07-13 10:39:50 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - This is the main function
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle):
+		return (o):
+}
