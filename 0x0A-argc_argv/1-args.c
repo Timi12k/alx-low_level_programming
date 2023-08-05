2a2d636a31b8ac5073918908e86742cd8ccfddf9
Timi12k <oluwatimilehinemmanuel62@gmail.com>
2023-08-04 18:36:38 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - A programme that print numbers
+ * @argc: Argument count
+ * @argv: Argument vector
+ * Return: 0
+**/
+
+int main(int argc, char *argv[])
+{
+	(void) *argv;
+	printf("%d\n", argc);
+	return (0);
+
+}
