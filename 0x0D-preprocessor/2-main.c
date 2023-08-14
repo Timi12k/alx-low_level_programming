c051382c87958eb16aadbab1af7955d49950a888
Timi12k <oluwatimilehinemmanuel62@gmail.com>
2023-08-14 12:24:39 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main- Programe to print file name
+ *Description: print file name from which file is compiled from
+ *Return: Always a sucess 
+ **/
+int main (void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
